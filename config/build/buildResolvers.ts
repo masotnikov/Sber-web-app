74605cee36975b3fca799258cdf9be71938b181f
masotnikov <masotnikov11@yandex.ru>
2025-01-11 16:00:09 +0300

    add fonts, normalize

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,6 +3,9 @@ import {BuildOptions} from "./types/types";
 
 export function buildResolvers(options: BuildOptions):  ResolveOptions {
     return {
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
         extensions: ['.tsx', '.ts', '.js'],
         alias: {
             '@': options.paths.src,
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './index.scss';
+import '@/shared/styles/index.scss';
 
 const App = () => {
     return (
         <div className='app'>
+            <h5>йцукенгшщзхъфывапролджэ</h5>
+            <h6>1234567890</h6>
             <h2>hello world!</h2>
             <div>{__PLATFORM__}</div>
 
