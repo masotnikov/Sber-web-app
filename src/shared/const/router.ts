d1c76fc80af9b4faea3d4e7896699dfcea00a5f2
masotnikov <masotnikov11@yandex.ru>
2025-01-25 11:12:31 +0300

    chore(router): added const

---
+++ src/shared/const/router.ts
@@ -0,0 +1,12 @@
+export enum AppRoutes {
+  MAIN = 'main',
+  SAVINGS = 'savings',
+  PAYMENTS = 'payments',
+  HISTORY = 'history',
+  NOT_FOUND = 'not_found'
+}
+
+export const getRouteMain = () => '/main'
+export const getRouteSavings = () => '/savings'
+export const getRoutePayments = () => '/payments'
+export const getRouteHistory = () => '/history'
