bcfc6e39a929f79061cf1d34e97576f2c075a205
masotnikov <masotnikov11@yandex.ru>
2025-01-25 11:18:43 +0300

    feat: added BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from "./app/App";
+import {BrowserRouter} from "react-router-dom";
 
 console.log('React application starts rendering...');
 const rootElement = document.getElementById('root');
@@ -10,7 +11,7 @@ if (!rootElement) {
 const root = createRoot(rootElement);
 
 root.render(
-    <>
+    <BrowserRouter>
       <App/>
-    </>
+    </BrowserRouter>
 );
