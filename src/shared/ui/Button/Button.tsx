c9b15b6f17d85068d5f868dd5463ff3a3dcbb9e7
masotnikov <masotnikov11@yandex.ru>
2025-01-26 20:05:02 +0300

    feat(component): add Button, styles, public api

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,18 @@
+import {ButtonHTMLAttributes, FC} from "react";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import cls from "./Button.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const {className, children, ...otherProps} = props;
+
+  return (
+    <button {...otherProps} className={classNames(cls.Button, {}, [className])}>
+      {children}
+    </button>
+  );
+};
+
