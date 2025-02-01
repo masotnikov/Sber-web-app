e7fef22436740124e8e46feab11ca05b030a0dd7
masotnikov <masotnikov11@yandex.ru>
2025-02-01 23:41:56 +0300

    test: add tests for classNames helper

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,27 @@
-// import { classNames } from "./classNames";
+import { classNames } from "./classNames";
 
 describe("classNames", () => {
-  test("classNames works", () => {
-    expect(true).toBe(true);
+  test("with first argument", () => {
+    expect(classNames("someClass")).toBe("someClass");
+  });
+
+  test("with additional classes", () => {
+    const expected = "someClass class1 class2";
+    expect(classNames("someClass", {}, ["class1 class2"])).toBe(expected);
+  });
+
+  test("with mods", () => {
+    const expected = "someClass class1 class2 hovered scrollable";
+    expect(classNames("someClass", { hovered: true, scrollable: true }, ["class1 class2"])).toBe(expected);
+  });
+
+  test("with mods false", () => {
+    const expected = "someClass class1 class2 scrollable";
+    expect(classNames("someClass", { hovered: false, scrollable: true }, ["class1 class2"])).toBe(expected);
+  });
+
+  test("with mods undefined", () => {
+    const expected = "someClass class1 class2 scrollable";
+    expect(classNames("someClass", { hovered: undefined, scrollable: true }, ["class1 class2"])).toBe(expected);
   });
 });
