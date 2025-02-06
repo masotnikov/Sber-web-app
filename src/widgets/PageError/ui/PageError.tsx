adee56e43ebc21b10e99d5ba62b62dac5688b4bc
masotnikov <masotnikov11@yandex.ru>
2025-02-06 14:58:15 +0300

    feat(component): added PageError

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+import {classNames} from "@/shared/lib/classNames/classNames";
+import cls from "./PageError.module.scss";
+import {Button} from "@/shared/ui/Button";
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = (props) => {
+  const { className } = props;
+
+  function handleReload (): void {
+    window.location.reload();
+  }
+
+  return (
+    <div className={classNames(cls.PageError, {}, [className])}>
+      <h1 className={cls.message}>
+        Произошла ошибка, обновите страницу!
+      </h1>
+      <Button onClick={handleReload} className={cls.button} type={"button"}>Перезагрузка страницы</Button>
+    </div>
+  );
+};
+
