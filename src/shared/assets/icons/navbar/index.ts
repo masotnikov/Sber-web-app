0cae383a82f436238b279b85551fab65f5df7129
masotnikov <masotnikov11@yandex.ru>
2025-02-07 15:27:42 +0300

    feat: added public api

---
+++ src/shared/assets/icons/navbar/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeIcon } from "./home.svg";
+export { default as PaymentsIcon } from "./payments.svg";
+export { default as OperationsIcon } from "./operations.svg";
+export { default as SavingIcon } from "./savings.svg";
