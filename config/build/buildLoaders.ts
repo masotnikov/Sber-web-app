099096e5970dcc06cf8ddbe2ba0e434d3020a6fd
masotnikov <masotnikov11@yandex.ru>
2025-02-15 00:05:55 +0300

    chore(webpack): refactoring loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,9 +2,9 @@ import { ModuleOptions } from "webpack";
 import { BuildOptions } from "./types/types";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshTypeScript from "react-refresh-typescript";
+import {buildCssLoader} from "./loaders/buildCssLoader";
 
-export function buildLoaders (options: BuildOptions): ModuleOptions["rules"] {
-  const isDev = options.mode === "development";
+export function buildLoaders ({ isDev }: BuildOptions): ModuleOptions["rules"] {
 
   const tsLoader = {
     exclude: /node_modules/,
@@ -59,26 +59,7 @@ export function buildLoaders (options: BuildOptions): ModuleOptions["rules"] {
   };
 
 
-  const cssLoader = {
-    test: /\.(css|s[ac]ss)$/i,
-    use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            namedExport: false,
-            exportLocalsConvention: "as-is",
-            auto: (resPath: string): boolean => Boolean(resPath.includes(".module.")),
-            localIdentName: isDev
-              ? "[path][name]--[local]--[hash:base64:4]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      "sass-loader",
-    ],
-  };
+  const cssLoader = buildCssLoader(isDev);
 
   return [
     assetLoader,
